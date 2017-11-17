40f2c0c1e43cb9ca7bd2d0167f5592dacd028f61
Chase Davis <chase.davis@gmail.com>
2017-11-17 15:16:06 -0600

    Some prod settings changes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,11 +6,8 @@ echo 'Running loaddata ...'
 python manage.py loaddata free_templates --verbosity=3
 python manage.py loaddata free_charts --verbosity=3
 
-if [ $ENV == "local" ]
-then
-  echo 'Creating superuser admin/admin (dev only)'
-  echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('admin', 'admin@example.com', 'admin'); user.first_name = 'Admin'; user.save();" | python manage.py shell
-fi
+echo 'Creating superuser admin/admin'
+echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('admin', 'admin@example.com', 'admin'); user.first_name = 'Admin'; user.save();" | python manage.py shell
 
 echo 'Collecting static ...'
 python manage.py collectstatic --noinput
